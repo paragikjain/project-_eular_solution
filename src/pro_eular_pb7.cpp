dede34b24f0efd4a9ada74b2e175d1b42e5b94d2
Parag Jain <40451840+paragikjain@users.noreply.github.com>
2020-03-06 05:31:01 +0530

    solution 8

--- src/pro_eular_pb7.cpp
+++ src/pro_eular_pb7.cpp
@@ -4,7 +4,7 @@ author: Parag Jain
 problem 7:
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
-Date: 6/3/2020
+Date: 5/3/2020
 */
 #include <iostream>
 #include <math.h>
